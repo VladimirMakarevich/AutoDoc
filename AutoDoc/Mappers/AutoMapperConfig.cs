4d9bf1592e09914e5c8bc0694dd3e22573e26929
VladiMakarevich <justadreampictures@gmail.com>
2017-05-16 17:30:26 +0300

    added automapper

---
+++ AutoDoc/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoDoc.Mappers
+{
+    public class AutoMapperConfig
+    {
+        //public static IMapper GetMapper(IContext context)
+        //{
+        //    MapperConfiguration config = new MapperConfiguration(RegisterMappings);
+        //    IMapper mapper = config.CreateMapper();
+        //    return mapper;
+        //}
+
+        //private static void RegisterMappings(IMapperConfigurationExpression config)
+        //{
+        //}
+    }
+}
--- AutoDoc/Startup.cs
+++ AutoDoc/Startup.cs
@@ -23,15 +23,15 @@ namespace AutoDoc
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
