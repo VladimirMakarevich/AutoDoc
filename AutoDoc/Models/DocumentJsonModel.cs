75227dee0269ba52477ab7ae116a01af9cacdd16
Vladimir Makarevich <business.makarevich@outlook.com>
2017-05-22 08:49:15 +0300

    refactoring models

--- AutoDoc/Models/DocumentJsonModel.cs
+++ AutoDoc/Models/DocumentJsonModel.cs
@@ -8,7 +8,6 @@ namespace AutoDoc.Models
     public class DocumentJsonModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         public List<BookmarkJsonModel> Bookmarks { get; set; }
     }
 }
